bf22276ef3162fdcadf7d413e1bf1f148029d2f9
Yago Tavares <yago.teixeira5@aluno.ce.gov.br>
2026-09-11 14:26:21 +0000

    projeto finalizado

---
+++ 3-bimestre/logica-de-programacao/projetos/projeto2/index.js
@@ -0,0 +1,69 @@
+const cliente = "Lucas Almeida"
+const produto = "Notebook Gamer"
+const preco = 4500
+const quantidade = 2
+const estoque = 10
+const valorPago = 9000
+const subtotal = preco * quantidade
+const descontoPercentual = 10
+const valorDesconto = subtotal * descontoPercentual / 100
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+
+let statusPedido = "Aguardando"
+let pagamentoStatus = "aguardo"
+let estoqueDisponivel = "aguardando"
+
+
+
+
+
+if (estoque >= quantidade){
+    estoqueDisponivel = "Sim"
+    statusPedido = "Pedido disponível para finalização" 
+    console.log(statusPedido)
+} else{
+    statusPedido = "Pedido não disponível para finalização"
+    estoqueDisponivel = "Não"
+    console.log("Pedido não pode ser finalizado por falta de estoque")
+}
+
+if (valorPago >= valorFinal) {
+    pagamentoStatus = "Pagamento aprovado" 
+    
+} else {
+    pagamentoStatus = "Pagamento insuficiente"
+}
+
+const resumo = `Cliente: ${cliente}
+Produto: ${produto}
+Preço: R$${preco}
+Quantidade: ${quantidade}
+Estoque: ${estoque}
+Valor Pago: R$${valorPago}
+Desconto Percentual: ${descontoPercentual}%
+Valor Desconto: R$ ${valorDesconto}
+Valor Final R$ ${valorFinal}
+Troco R$ ${troco}
+Pagamento: ${pagamentoStatus}
+Estoque disponível: ${estoqueDisponivel}
+Status do pedido: ${statusPedido}`
+
+
+module.exports = {
+    cliente,
+    produto,
+    preco,
+    quantidade,
+    estoque,
+    valorPago,
+    subtotal,
+    estoqueDisponivel,
+    descontoPercentual,
+    valorDesconto,
+    valorFinal,
+    pagamentoStatus,
+    troco,
+    statusPedido,
+    resumo
+}
